7d2fecb90570821ce0179edee61f304424704c00
CAROLINAMAIQUEZ <caromaiquez@hotmail.com>
2023-01-20 15:57:49 -0300

    carolina

---
+++ ticmas final.js
@@ -0,0 +1,66 @@
+// Variables
+const formulario = document.querySelector('#formulario');
+const nombre = document.querySelector('#nombre');
+const edad = document.querySelector('#mail');
+const mensaje = document.querySelector('#mensaje');
+const enviar = document.querySelector('#enviar');
+const errores = document.querySelector('#errores');
+let mensajesErrores = [];
+
+// Funciones
+function validar (evento) {
+    // Evitar que se envie el formulario
+    evento.preventDefault();
+
+    // Vacia los mensajesErrores antes de rellenarlo nuevamente
+    mensajesErrores = [];
+
+    // VALIDACIONES
+
+    // Nombre es obligatorio
+
+    if (nombre.value.trim().length === 0) {
+        mensajesErrores = mensajesErrores.concat('Nombre es un campo obligatorio');
+    }
+
+    // Nombre carácteres válidos
+
+    if (!/^[a-zA-Z0-9]*$/.exec(nombre.value.trim())) {
+        mensajesErrores = mensajesErrores.concat('Nombre no tiene carácteres válidos');
+    }
+
+    // Edad debe ser números
+
+    if (isNaN(mail.value.trim())) {
+        mensajesErrores = mensajesErrores.concat('El mail debe tener caracteres validos');
+    }
+
+    // Comprobamos que el mensaje tiene un mínimo de 10 carácteres
+
+    if (mensaje.value.trim().length < 10) {
+        mensajesErrores = mensajesErrores.concat('Mensaje demasiado corto');
+    }
+
+    // ENVIAR O MOSTRAR MENSAJES
+    if (mensajesErrores.length === 0) {
+        // Enviamos el formulario si no hay errores
+        formulario.submit();
+    } else {
+        // Muestro los errores
+        errores.textContent = '';
+        mensajesErrores.forEach(function (mensaje) {
+            const miLi = document.createElement('li');
+            miLi.textContent = mensaje;
+            errores.appendChild(miLi);
+        });
+    }
+}
+
+// Eventos
+formulario.addEventListener('submit', validar);
+
+// Inicio
+
+
+  
+(jQuery, window, document);
